1f43d1ae6c48e367e8f60d4cbdc4310e5b01d675
ilya_bobrov <ilya.bobrov2013@yandex.ru>
2019-11-13 14:08:37 +1000

    update task R

--- CatsR/main.c
+++ CatsR/main.c
@@ -1,4 +1,4 @@
-///ручная проверка R
+//bobrov_R
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,25 +6,21 @@
 #define MAX_SIZE 499500 + 1
 #define BIG_NUM 11
 
+#pragma pack(push,1)
 typedef struct Graph{
-    int id;         //порядковый номер ребра
+    int id;
     short weight;
 }Graph;
+#pragma pack(pop)
 
 Graph list_s[MAX_SIZE];
 int distance[MAX_COUNT * 10];
 int prev_v[MAX_COUNT];
 int v_count, r_count;
 
-/*для линейного массива находим позицию
- * 0
- * 0 0
- * 0 0 0
- * 0 0 0 0
-*/
 int getIndex(int x, int y) {
     if (x > y)
-        return (x * (x - 1)) / 2 + y; //сумма последовательности
+        return (x * (x - 1)) / 2 + y;
     else
         return (y * (y - 1)) / 2 + x;
 }
@@ -35,7 +31,6 @@ int main () {
     fscanf(input, "%d", &v_count);
     fscanf(input, "%d", &r_count);
 
-    //init
     int index;
     for (int i = 0; i < r_count; i++) {
         static int v1, v2, weight;
@@ -50,32 +45,27 @@ int main () {
     }
     int min_len, next_v, v_A = 0;
 
-    //prim
     while (1) {
-        distance[v_A] = 0; //дистанция до самой вершины = 0
+        distance[v_A] = 0;
 
-        //бежим по вершинам
         for (int v_B = 0; v_B < v_count; v_B++) {
             if (v_A == v_B) continue;
             index = getIndex(v_A, v_B);
-            //первое условие для проверки ребра впринципе, второе для нахождения наименьшего ребра
             if (list_s[index/*(v_A, v_B)*/].weight > 0 && list_s[index/*(v_A, v_B)*/].weight < distance[v_B]) {
-                prev_v[v_B] = v_A; //найденой вершине обозначили ее множество
+                prev_v[v_B] = v_A;
                 distance[v_B] = list_s[index].weight;
             }
         }
 
-        //init
         next_v = v_A;
         min_len = BIG_NUM;
-        //бежим по всем вершинам и ищем кротчайший путь
         for (int v_B = 0; v_B < v_count; v_B++)
             if (distance[v_B] != 0 && distance[v_B] < min_len ) {
                 next_v = v_B;
                 min_len = distance[v_B];
             }
 
-        if (next_v == v_A) // если мы не нашли новой вершины - выходим
+        if (next_v == v_A)
             break;
         else {
             fprintf(output,"%d\n", list_s[ getIndex(prev_v[next_v], next_v) ].id);
@@ -83,10 +73,9 @@ int main () {
         }
     }
 
-    fflush(input);
     fflush(output);
-    fclose(input);
-    fclose(output);
+    fflush(input);
+
     free(output);
     free(input);
 
